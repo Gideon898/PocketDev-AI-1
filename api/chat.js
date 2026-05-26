50c0a1941655efb7de510b362d22cabc5747c1b0
Flight <andakurogideon99@gmail.com>
2026-05-26 10:12:25 -0700

    Create chat.js

---
+++ api/chat.js
@@ -0,0 +1,71 @@
+export default async function handler(req, res){
+
+  if(req.method !== "POST"){
+
+    return res.status(405).json({
+      error:"Only POST allowed"
+    });
+  }
+
+  try{
+
+    const { message } =
+    req.body;
+
+    const response =
+    await fetch(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+
+        method:"POST",
+
+        headers:{
+          "Authorization":
+          `Bearer ${process.env.OPENROUTER_API_KEY}`,
+
+          "Content-Type":
+          "application/json"
+        },
+
+        body: JSON.stringify({
+
+          model:
+          "openai/gpt-4o-mini",
+
+          messages:[
+            {
+              role:"user",
+              content:message
+            }
+          ]
+        })
+      }
+    );
+
+    const data =
+    await response.json();
+
+    console.log(data);
+
+    res.status(200).json({
+
+      reply:
+      data.choices?.[0]
+      ?.message?.content ||
+
+      data.error?.message ||
+
+      "No response"
+    });
+
+  }
+
+  catch(error){
+
+    console.log(error);
+
+    res.status(500).json({
+      error:"Server error"
+    });
+  }
+}
